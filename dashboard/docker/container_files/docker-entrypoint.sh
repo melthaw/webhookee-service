06038b274415bbfc8923a390243c5d886da9315c
chenxue <melthaw@gmail.com>
2018-08-28 14:54:18 +0800

    remove dashboard

--- dashboard/docker/container_files/docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-
-while true; do sleep 10000; done
--- dashboard/e2e/app.po.ts
+++
@@ -1,7 +0,0 @@
-import { browser, element, by } from 'protractor';
-
-export class MorpheusPage {
-  navigateTo() {
-    return browser.get('/');
-  }
-}
